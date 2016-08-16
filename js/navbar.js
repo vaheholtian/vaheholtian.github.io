3422722a1b6715cf07ec45d06e02063655badb4a
vaheholtian <vaheholtian@gmail.com>
2016-08-17 00:48:54 +0300

    Updates

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,2 @@
-/* window.addEventListener("load", function() { fixNav();}); */
-
-function fixNav() {
-   if (document.getElementById("myNav").style.height == "100%") {
-     document.getElementById("myNav").style.height = "0%";    
-   }
-  else {
-  document.getElementById("myNav").style.height = "100%";
-  }
-}
+function fixNav(){if(document.getElementById("myNav").style.height=="100%"){document.getElementById("myNav").style.height="0%";}
+else{document.getElementById("myNav").style.height="100%";}}/* window.addEventListener("load", function() { fixNav();}); */
